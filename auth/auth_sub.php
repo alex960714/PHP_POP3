91b10f10922b8c652423986e67dc1ca054795146
alex960714 <alex_9607@mail.ru>
2016-11-01 01:41:52 +0300

    Auth procedure init

--- auth/auth_sub.php
+++ auth/auth_sub.php
@@ -1,4 +1,29 @@
 <?php
+define('CRLF',"\r\n");
+
+$login=htmlspecialchars($_GET["login"]);
+$client=htmlspecialchars($_GET["client"]);
+$password=htmlspecialchars($_GET["password"]);
+
+$email=$login."@".$client;
+
+$fp=fsockopen("pop.".$client,110,$errno,$errstr,10);
+echo fgets($fp,1024).'<br />';
+
+/*fputs($fp,"STLS ".CRLF);
+echo fgets($fp,1024).'<br />';*/
+
+fputs($fp,"USER ".$login.CRLF);
+echo fgets($fp,1024).'<br />';
+
+fputs($fp,"PASS ".$password.CRLF);
+echo fgets($fp,1024).'<br />';
+
+fputs($fp,"QUIT ".CRLF);
+echo fgets($fp,1024).'<br />';
+
+fclose($fp);
+?>
 /**
  * Created by PhpStorm.
  * User: Александр
