5a47e069957398254798b14ea5fb35c1ef9a0dd6
Nesterov Alexander <nesoldr@mail.ru>
2016-12-05 17:50:31 +0300

    Git merge Alex #1

---
+++ auth/auth_sub.php
@@ -0,0 +1,32 @@
+<?php
+define('CRLF',"\r\n");
+
+$login=htmlspecialchars($_GET["login"]);
+$client=htmlspecialchars($_GET["client"]);
+$password=htmlspecialchars($_GET["password"]);
+
+$email=$login."@".$client;
+$host="pop.".$client;
+$fp=fsockopen($host,110,$errno,$errstr,10);
+echo fgets($fp,1024).'<br />';
+
+/*fputs($fp,"STLS ".CRLF);
+echo fgets($fp,1024).'<br />';*/
+
+fputs($fp,"USER ".$login.CRLF);
+echo fgets($fp,1024).'<br />';
+
+fputs($fp,"PASS ".$password.CRLF);
+echo fgets($fp,1024).'<br />';
+
+fputs($fp,"QUIT ".CRLF);
+echo fgets($fp,1024).'<br />';
+
+fclose($fp);
+?>
+/**
+ * Created by PhpStorm.
+ * User: Александр
+ * Date: 31.10.2016
+ * Time: 17:38
+ */
